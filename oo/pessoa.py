1f98f4fcc2ab4ec1e52b45daeba3c45c02569ff9
KarenTeixeira <karentteixeira@gmail.com>
2020-10-02 17:53:01 -0300

    Criando atributo de instância/dado, complexo e  dinâmico

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,13 +1,20 @@
 class Pessoa:
-    def __init__(self, nome=None, idade=35):
+    def __init__(self, *filhos, nome=None, idade=35):
         self.nome = nome
         self.idade = idade
+        self.filhos = list(filhos)
 
     def cumprimentar(self):
         return f'Olá {self.nome}!!'
 
 
 if __name__ == '__main__':
-    p = Pessoa('Karen', 28)
-    print(p.cumprimentar())
-    print(p.idade)
+    karen = Pessoa(nome='Karen', idade=28)
+    pedro = Pessoa(nome='Pedro', idade=7)
+    julia = Pessoa(nome='Julia', idade=4)
+    joao = Pessoa(pedro,julia, nome='João', idade=40)
+    luiz = Pessoa(karen, nome='Luiz', idade=57)
+    print(luiz.nome)
+    print(luiz.cumprimentar())
+    joao.sobrenome = 'Silva'
+    print(luiz.__dict__)
